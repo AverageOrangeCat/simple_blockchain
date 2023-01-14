9101d7ecfbeb1399209ce4f5d3b817e832a096a6
KuengNoel <noel.kueng@ik.me>
2023-01-14 22:05:05 +0100

    Implementation of `Block`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod block;
+use block::Block;
+
 fn main() {
-    println!("Hello, world!");
+    let mut block = Block {
+        previous_hash: String::new(),
+        current_hash: String::new(),
+        content: String::from("Hello, World!"),
+        nounce: 0
+    };
+
+    block.mine_block();
+    block.debug(String::new());
 }
